a75d42aea3e11a68abd0663fd881ccc55ea4e0aa
Mahmoud Ahmed <mahmoud.ahmed@diligentsource.com>
2025-02-17 22:49:58 +0200

    add home page

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Enable error reporting for development
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Simple test message
+echo "Welcome to NYT Article Explorer!";
